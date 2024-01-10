42fefb633ca1f26e60a731b1710ce3440b823a8e
unknown <maxence.Ab@gmail.com>
2024-01-10 14:21:42 +0100

    maj string.h

--- s_libc_project_B_2/include/s_string.h
+++ s_libc_project_B_2/include/s_string.h
@@ -1,2 +1,8 @@
-extern int s_strlen(char*);
+#ifndef S_STRING_H
+#define S_STRING_H
+
+extern int s_strlen(char *str);
+extern void s_strcpy(char *dest, const char *src);
+
+#endif /* S_STRING_H */
 
